ee02c1a5a43dc2621e2a47d72240c6cc9845def3
Mikkel Ricky <rimi@aarhus.dk>
2016-02-03 14:36:22 +0100

    Added Instagram API hack

--- modules/brilleappen/src/Controller/EventController.php
+++ modules/brilleappen/src/Controller/EventController.php
@@ -7,10 +7,13 @@
 namespace Drupal\brilleappen\Controller;
 
 use Drupal\node\Entity\Node;
+use Drupal\file\Entity\File;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Session\AccountInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Drupal\Core\Database\Query\Condition;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 class EventController extends ControllerBase {
   /**
@@ -48,9 +51,36 @@ class EventController extends ControllerBase {
       $node->field_gg_files->appendItem($file);
       $node->save();
 
+      $this->push($file, $node);
+
       $this->sendResponse([ 'status' => 'OK', 'message' => 'File added to event "' . $node->getTitle() . '"' ]);
     } catch (\Exception $ex) {
-      $this->sendResponse([ 'status' => 'ERROR', 'message' => $ex->getMessage() ], 400);
+      $this->sendResponse([ 'status' => 'ERROR', 'message' => $ex->getMessage(), 'type' => get_class($ex) ], 400);
+    }
+  }
+
+  private function push(File $file, Node $event) {
+    if ($event->field_gg_instagram_push->value) {
+      $username = $event->get('field_gg_instagram_username')->value;
+      $password = $event->get('field_gg_instagram_password')->value;
+      $caption = $event->get('field_gg_instagram_caption')->value;
+      $path = \Drupal::service('file_system')->realpath($file->getFileUri());
+
+      $igDataPath = file_directory_temp() . '/igdata/';
+      file_prepare_directory($igDataPath, FILE_CREATE_DIRECTORY);
+      $igDataPath .= '/';
+      $instagram = new \Instagram($username, $password, FALSE, $igDataPath);
+      try {
+        $instagram->login();
+        $instagram->uploadPhoto($path, $caption);
+        $instagram->logout();
+      } finally {
+        // Remove instagram cookies.
+        $cookiesFilename = 'cookies.dat';
+        if (file_exists($igDataPath . '/' . $cookiesFilename)) {
+          unlink($igDataPath . '/' . $cookiesFilename);
+        }
+      }
     }
   }
 
